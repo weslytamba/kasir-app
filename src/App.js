520928a13b7d15d68504f05ec3e592d7fd702550
weslytamba <weslytamba16@gmail.com>
2025-09-02 10:49:08 +0700

    Deskripsi perubahan terbaru

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Col } from "react-bootstrap";
 import { Container } from "react-bootstrap";
 import { API_URL } from "./utils/constants";
 import axios from "axios";
+import swal from "sweetalert";
 
 
 export default class App extends Component {
@@ -13,20 +14,45 @@ export default class App extends Component {
 
     this.state = {
       menus: [],
-      categoriYangDipilih: 'Makanan'
+      categoriYangDipilih: 'Makanan',
+      keranjangs: []
     }
   }
 
   componentDidMount() {
     axios
-    .get(API_URL +"products?category.nama=" + this.state.categoriYangDipilih)
-    .then(res => {
+      .get(API_URL + "products?category.nama=" + this.state.categoriYangDipilih)
+      .then(res => {
         const menus = res.data;
         this.setState({ menus });
       })
       .catch(error => {
         console.log("Error ya ", error);
+      });
+
+      axios
+      .get(API_URL + "keranjangs")
+      .then((res) => {
+        const keranjangs = res.data;
+        this.setState({ keranjangs });
       })
+      .catch(error => {
+        console.log("Error ya ", error);
+      });
+  }
+
+  componentDidUpdate(prevState){
+    if(this.state.keranjangs !== prevState.keranjangs){
+      axios
+      .get(API_URL + "keranjangs")
+      .then((res) => {
+        const keranjangs = res.data;
+        this.setState({ keranjangs });
+      })
+      .catch(error => {
+        console.log("Error ya ", error);
+      });
+    }
   }
 
   changeCategory = (value) => {
@@ -36,8 +62,8 @@ export default class App extends Component {
     })
 
     axios
-    .get(API_URL +"products?category.nama=" + value)
-    .then(res => {
+      .get(API_URL + "products?category.nama=" + value)
+      .then((res) => {
         const menus = res.data;
         this.setState({ menus });
       })
@@ -47,15 +73,69 @@ export default class App extends Component {
 
   }
 
+  masukKeranjang = (value) => {
+    axios
+      .get(API_URL + "keranjangs?product.id=" + value.id)
+      .then((res) => {
+        if (res.data.length === 0) {
+          const keranjang = {
+            jumlah: 1,
+            total_harga: value.harga,
+            product: value
+          };
+
+
+          axios
+            .post(API_URL + "keranjangs", keranjang)
+            .then((res) => {
+              swal({
+                title: "Sukses Masuk Keranjang",
+                text: "Sukses Masuk Keranjang! " + keranjang.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1000,
+              });
+            })
+            .catch(error => {
+              console.log("Error ya ", error);
+            })
+        }else {
+          const keranjang = {
+            jumlah: res.data[0].jumlah+1,
+            total_harga: res.data[0].total_harga + value.harga,
+            product: value,
+          };
+
+          axios
+            .put(API_URL + "keranjangs/"+res.data[0].id, keranjang)
+            .then((res) => {
+              swal({
+                title: "Sukses Masuk Keranjang",
+                text: "Sukses Masuk Keranjang " + keranjang.product.nama,
+                icon: "success",
+                button: false,
+                timer: 1000,
+              });
+            })
+            .catch(error => {
+              console.log("Error ya ", error);
+            })
+        }
+      })
+      .catch(error => {
+        console.log("Error ya ", error);
+      })
+  }
+
   render() {
-    const { menus, categoriYangDipilih } = this.state
+    const { menus, categoriYangDipilih, keranjangs } = this.state
     return (
       <div className="App">
         <NavbarComponent> </NavbarComponent>
         <div className="mt-3">
           <Container fluid>
             <Row>
-              <ListCategories changeCategory={this.changeCategory} categoriYangDipilih={categoriYangDipilih}/>
+              <ListCategories changeCategory={this.changeCategory} categoriYangDipilih={categoriYangDipilih} />
               <Col>
                 <h4><strong>Daftar Produk</strong></h4>
                 <hr />
@@ -64,11 +144,12 @@ export default class App extends Component {
                     <Menus1
                       key={menu.id}  // Pindahkan key ke sini (sebagai prop)
                       menu={menu}
+                      masukKeranjang={this.masukKeranjang}
                     />
                   ))}
                 </Row>
               </Col>
-              <Hasil />
+              <Hasil keranjangs={keranjangs}/>
             </Row>
           </Container>
         </div>
--- src/component/Hasil.js
+++ src/component/Hasil.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
-import { Col } from 'react-bootstrap'
+import { Badge, Col, ListGroup } from 'react-bootstrap'
+import { numberWithCommas } from '../utils/comacode';
 
 export default class Hasil extends Component {
     render() {
+        const { keranjangs } = this.props
         return (
             <Col md={3} mt={2}>
                 <h4><strong>Hasil</strong></h4>
                 <hr />
+                {keranjangs.length !== 0 && (
+                    <ListGroup variant="flush">
+                        {keranjangs.map((menuKeranjang) => (
+                            <ListGroup.Item>
+                                <row>
+                                    <Col xs={2}>
+                                        <h8>
+                                            <Badge pill variant="success">
+                                                {menuKeranjang.jumlah}
+                                            </Badge>
+                                        </h8>
+                                    </Col>
+                                    <Col>
+                                        <h5>{menuKeranjang.product.nama}</h5>
+                                        <p>Rp. {numberWithCommas(menuKeranjang.product.harga)}</p>
+                                    </Col>
+                                    <Col>
+                                       <strong className='float-right'>
+                                         Rp.{numberWithCommas(menuKeranjang.total_harga)}
+                                       </strong>
+                                    </Col>
+                                </row>
+                            </ListGroup.Item>
+                        ))}
+                    </ListGroup>
+                )}
             </Col>
         );
     }
